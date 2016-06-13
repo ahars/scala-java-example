d8dc26595edeb751eaade252ef8361146d1872f9
Antoine HARS <ahars@ippon.fr>
2016-06-13 10:46:42 +0200

    add a HelloWorld Java Class

---
+++ src/main/java/com/ahars/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ahars.hello;
+
+/**
+ * Created by ahars on 13/06/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
